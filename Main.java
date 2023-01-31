8c0a161e273b09786a0c448bcab16bec6673ca7a
obada1234 <b00090191@aus.edu>
2023-01-31 17:50:41 +0400

    Feature B added

--- Main.java
+++ Main.java
@@ -5,6 +5,12 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		
+		featureB();
+	}
+
+	private static void featureB() {
+		System.out.println("called featureB()");
 	}
 
 }
